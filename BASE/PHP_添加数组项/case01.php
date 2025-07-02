fedcdeac5dbdaa2428b39a7e5400355e760b73da
lirongyaoper <lirongyaoper@163.com>
2025-07-03 07:11:36 +0800

    update data at 20250703071136

---
+++ BASE/PHP_添加数组项/case01.php
@@ -0,0 +1,6 @@
+<?php
+$fruits = array("Apple", "Banana", "Cherry");
+array_push($fruits, "Orange", "Kiwi", "Lemon");
+
+//Output the array:
+var_dump($fruits);
---
+++ BASE/PHP_添加数组项/case02_向关联数组中添加多个项.php
@@ -0,0 +1,10 @@
+<?php
+//$cars = array("brand" => "BYD", "model" => "Mustang");
+//$cars += ["color" => "red", "year" => 1964];
+//var_dump($cars);
+
+$cars = array("brand" => "Ford", "model" => "Mustang");
+
+$cars["color"] = "red";
+$cars["year"] = 1946;
+var_dump($cars);
