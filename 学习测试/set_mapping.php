5e4f3235a6fdacd23419e9828c4cfa754fc193b3
lirongyaoper <lirongyaoper@163.com>
2025-07-08 12:33:20 +0800

    update data at 20250708123320

--- 学习测试/set_mapping.php
+++ 学习测试/set_mapping.php
@@ -1,101 +1,21 @@
 <?php
 $data = [
-    [
-        'catid' => 6,
-        'type' => 0,
-        'catdir' => 'yixueyuandi',
-        'arrchildid' => '6,9,10,16'
-    ],
-    [
-        'catid' => 7,
-        'type' => 0,
-        'catdir' => 'xianyansuiyu',
-        'arrchildid' => '7'
-    ],
-    [
-        'catid' => 8,
-        'type' => 1,
-        'catdir' => 'guanyuziji',
-        'arrchildid' => '8'
-    ],
-    [
-        'catid' => 9,
-        'type' => 0,
-        'catdir' => 'yixinyiyi',
-        'arrchildid' => '9'
-    ],
-    [
-        'catid' => 10,
-        'type' => 0,
-        'catdir' => 'xuewuzhijing',
-        'arrchildid' => '10'
-    ],
-    [
-        'catid' => 11,
-        'type' => 0,
-        'catdir' => 'zhiqurensheng',
-        'arrchildid' => '11,17,18,19,20'
-    ],
-    [
-        'catid' => 12,
-        'type' => 0,
-        'catdir' => 'diandishenghuo',
-        'arrchildid' => '12,13,14,21,22'
-    ],
-    [
-        'catid' => 13,
-        'type' => 0,
-        'catdir' => 'babaizhijiao',
-        'arrchildid' => '13'
-    ],
-    [
-        'catid' => 14,
-        'type' => 0,
-        'catdir' => 'fengyunbianhuan',
-        'arrchildid' => '14'
-    ],
-    [
-        'catid' => 16,
-        'type' => 0,
-        'catdir' => 'yixuejiankang',
-        'arrchildid' => '16'
-    ],
-    [
-        'catid' => 17,
-        'type' => 0,
-        'catdir' => 'phpxiangguan',
-        'arrchildid' => '17'
-    ],
-    [
-        'catid' => 18,
-        'type' => 0,
-        'catdir' => 'suanfashuju',
-        'arrchildid' => '18'
-    ],
-    [
-        'catid' => 19,
-        'type' => 0,
-        'catdir' => 'caozuoxitong',
-        'arrchildid' => '19'
-    ],
-    [
-        'catid' => 20,
-        'type' => 0,
-        'catdir' => 'shiyonggongju',
-        'arrchildid' => '20'
-    ],
-    [
-        'catid' => 21,
-        'type' => 0,
-        'catdir' => 'gujianqingshen',
-        'arrchildid' => '21'
-    ],
-    [
-        'catid' => 22,
-        'type' => 0,
-        'catdir' => 'yangyuzhien',
-        'arrchildid' => '22'
-    ]
+    ['catid' => 6, 'type' => 0, 'catdir' => 'yixueyuandi', 'arrchildid' => '6,9,10,16'],
+    ['catid' => 7, 'type' => 0, 'catdir' => 'xianyansuiyu', 'arrchildid' => '7'],
+    ['catid' => 8, 'type' => 1, 'catdir' => 'guanyuziji', 'arrchildid' => '8'],
+    ['catid' => 9, 'type' => 0, 'catdir' => 'yixinyiyi', 'arrchildid' => '9'],
+    ['catid' => 10, 'type' => 0, 'catdir' => 'xuewuzhijing', 'arrchildid' => '10'],
+    ['catid' => 11, 'type' => 0, 'catdir' => 'zhiqurensheng', 'arrchildid' => '11,17,18,19,20'],
+    ['catid' => 12, 'type' => 0, 'catdir' => 'diandishenghuo', 'arrchildid' => '12,13,14,21,22'],
+    ['catid' => 13, 'type' => 0, 'catdir' => 'babaizhijiao', 'arrchildid' => '13'],
+    ['catid' => 14, 'type' => 0, 'catdir' => 'fengyunbianhuan', 'arrchildid' => '14'],
+    ['catid' => 16, 'type' => 0, 'catdir' => 'yixuejiankang', 'arrchildid' => '16'],
+    ['catid' => 17, 'type' => 0, 'catdir' => 'phpxiangguan', 'arrchildid' => '17'],
+    ['catid' => 18, 'type' => 0, 'catdir' => 'suanfashuju', 'arrchildid' => '18'],
+    ['catid' => 19, 'type' => 0, 'catdir' => 'caozuoxitong', 'arrchildid' => '19'],
+    ['catid' => 20, 'type' => 0, 'catdir' => 'shiyonggongju', 'arrchildid' => '20'],
+    ['catid' => 21, 'type' => 0, 'catdir' => 'gujianqingshen', 'arrchildid' => '21'],
+    ['catid' => 22, 'type' => 0, 'catdir' => 'yangyuzhien', 'arrchildid' => '22']
 ];
 
 $m = 'index';
